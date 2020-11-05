8ef2b0c53db47f82aafc368b33c721c2a9ab0659
coderzry <18348738308@163.com>
2020-11-05 09:32:02 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// 按需导入组件
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 挂载到vue原型 每个组件都可使用
+Vue.prototype.$message = Message

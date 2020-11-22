884ae5883728e8afb3c463ef10ce47e88e84c539
coderzry <18348738308@163.com>
2020-11-22 14:51:16 +0800

    完成了报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params'
 import GoodsList from '../components/goods/List.vue'
 import AddGoods from '../components/goods/Add.vue'
 import Order from '../components/order/Order'
+import Report from '@/components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -64,6 +65,10 @@ const routes = [
       {
         path: '/orders',
         component: Order
+      },
+      {
+        path: '/reports',
+        component: Report
       }
     ]
   }
